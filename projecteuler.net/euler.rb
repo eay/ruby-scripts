153558df74f05e7123fddbd8188c11905a8871b3
Eric Young <eay@pobox.com>
2009-11-16 22:02:15 +1000

    problem 82.
    Not too hard, just work backwards by columns.

--- projecteuler.net/euler.rb
+++ projecteuler.net/euler.rb
@@ -37,7 +37,52 @@ def problem_81
   walk.call(grid.dup,0,0)
 end
 
+# Tweaking of problem_81, to solve I work from the last column back.
+# We create a new column which is the current value + the minium
+# cost to the next column.  To work out this minium cost, we can
+# move up/down an arbitary number then go right.
+def problem_82
+  grid = open("matrix.txt").each_line.to_a.map do |l|
+    l.chomp.split(/,/).map(&:to_i)
+  end.transpose
+
+  # return a new row with the minimum values achievable
+  # moving from x0 to x1
+  right_min = lambda do |x0,x1|
+    len = x0.length
+    ret = []
+    (0...len).each do |y|
+      # For each y item we calcultate the possible scores of
+      # moving up(n)/down(n) then right
+      delta = []
+      delta[y] = 0
+      (y-1).downto(0).each do |yy|
+        delta[yy] = delta[yy+1] + x0[yy]
+      end
+      (y+1).upto(len-1).each do |yy|
+        delta[yy] = delta[yy-1] + x0[yy]
+      end
+      # delta is the up/down cost, now add the value of the right element
+      delta.each_index do |i|
+        delta[i] = delta[i] + x1[i]
+      end
+      # Delta now contains possible values to the next row, put the smallest
+      # in the return spot
+      ret[y] = x0[y] + delta.min
+    end
+    ret
+  end
+
+  # Start at the back and keep on generating new 'final' columns
+  # # Start at the back and keep on generating new 'final' columns.
+  last = right_min.call(grid[-2],grid[-1])
+  (grid.length-3).downto(0).each do |x|
+    last = right_min.call(grid[x],last)
+  end
+  last.min
+end
+
 if __FILE__ == $0
-  p problem_81
+  p problem_82
 end
 
