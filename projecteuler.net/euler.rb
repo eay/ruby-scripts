df2e88841ede53ef079bd70987a6ce5bf68dedfc
Eric Young <eay@pobox.com>
2009-12-13 00:15:15 +1000

    Problem 111.
    It could perhaps be improved, but at 3.1sec, this is fast enough

--- projecteuler.net/euler.rb
+++ projecteuler.net/euler.rb
@@ -5,60 +5,66 @@ require 'groupings.rb'
 #require 'polynomial.rb'
 #require 'point.rb'
 
-def problem_110(num_digits = 10)
+# To solve this one, we make a template of 10 of the digit we are checking
+# and then, for the number of 'replacements', permutate that number from
+# all possible replacement locations.
+# Then for each 'set' of replacement locations, try all possible values.
+# We could prune this better, but we simply discard leading zero's and if
+# the last digit is not in [1,3,7,9].
+# We could perhaps do the sum % 3 != 0 test, but it would probably not 
+# improve things much.
+def problem_111(num_digits = 10)
   digits = (0..9).to_a
   last = [1,3,7,9]
-
-  def p_110(missing,padding,padding_num,&block)
-    (missing + digit_array).permutations(num_digits) do |g|
-      yield g
-    end
-  end
-
   total = 0
   hits = {}
-  # Since all the same 'digit' is not prime, start at d-1
   (0..9).each do |digit|
     d_found,d_sum = 0,0
-    (num_digits - 1).downto(1) do |digit_length|
-      sum = digit_length * digit
-      digit_array =  [digit] * digit_length
-      (digits - [digit]).combination(num_digits - digit_length) do |missing|
-        next if missing.reduce(sum,&:+) % 3 == 0
-#        puts "d=#{digit} #{([digit] * digit_length).inspect}#{missing.inspect}"
-        
-        # Must do this better
-        # Loop over all 'missing' permutation, but insert the digit in
-        # the gaps
-        puts "missing = #{missing.inspect}"
-        missing.permutation do |perm|
-          puts "perm = #{perm.inspect}"
-          # We now need to 'fill in the gaps'
-          (perm.length + 1).groupings do |g|
-            ex = perm.length + 1 - g.length
-            g += [0] * ex
-            puts "g = #{g.inspect}"
-            gg = g.map {|n| [digit] * n}
-            # gg is what will fill in the gaps
-            puts "gg = #{gg}"
-            gg.permutation do |dp|
-              dpp = dp.zip(perm).flatten.compact
-              puts "dpp = #{dpp.inspect}"
-              next unless last.find(dpp[-1]) && dpp[0] != 0
-              test = dpp.join.to_i
-              next if hits[test]
-              if test.prime?
-                d_found += 1
-                d_sum += test
-                hits[test] = true
-              end
-            end
+    array = [digit] * num_digits
+    (1).upto(num_digits-1) do |num_replacements|
+      # We have an array of the search digit, then we
+      # generate 'n' length permutations of all available locations
+      # and place our 'replacement' numbers in those locations.
+      rep_finish = 10**num_replacements
+
+      slots = Array.new(num_digits) {|i| i}
+      # We use p_seen as a simple way to stop the duplicate value
+      # problem, since we permutate over all replacement combinations
+      # and we try all values each time.
+      p_seen = {}
+      slots.permutation(num_replacements) do |rep|
+        # Remove permutation duplication
+        reps = rep.sort
+        next if p_seen[reps]
+        p_seen[reps] = true
+
+        # Put replacement values in the 'rep' locations.
+        # We do this by just incrementing a number and chopping it up into
+        # the locations
+        test = array.dup
+        v = 0 # want it visable outside
+        0.upto(10**num_replacements) do |rep_value|
+          rep.each do |i|
+            v = rep_value % 10
+            break if v == digit
+            test[i] = v
+            rep_value /= 10
+          end
+          next if v == digit
+          # no leading 0's
+          # skip if the last digit stops us being prime
+          next unless last.member? test[-1]
+          next if test[0] == 0
+          if (t = test.join.to_i).prime?
+            d_found += 1
+            d_sum += t
+            hits[t] = true
           end
         end
       end
       if d_found > 0
         total += d_sum
-        puts "M(#{num_digits},#{digit})=#{digit_length} " +
+        puts "M(#{num_digits},#{digit})=#{num_digits - num_replacements} " +
           "N(#{num_digits},#{digit})=#{d_found} " +
           "S(#{num_digits},#{digit})=#{d_sum}"
         break 
@@ -68,8 +74,9 @@ def problem_110(num_digits = 10)
   total
 end
 
+def problem_112
 
 if __FILE__ == $0
-  p problem_110(4)
+  p problem_110
 end
 
