a94a98720945c2ed93cf6189c762b91e0b69aa1e
Eric Young <eay@rsasecurity.com>
2017-03-08 18:54:35 +1000

    Hande missing problems

--- projecteuler.net/euler-all.rb
+++ projecteuler.net/euler-all.rb
@@ -10,11 +10,18 @@ Dir["euler*.rb"].each do |file|
 end
 
 if __FILE__ == $0
+  missing = []
   1.upto(122) do |p|
     prob = "problem_#{p}"
+    unless self.private_methods.member? prob.to_sym
+      missing << prob
+      next 
+    end
     puts "#{prob} => #{self.send(prob)}"
   end
 #  private_methods.select {|p| /^problem_\d+$/.match p}.map(&:to_s).sort_by {|p| a= p.match(/\d+$/)[0].to_i; puts a; a}.each do |prob|
 #    puts "#{prob} => #{self.send(prob)}"
 #  end
+
+  puts "missing questiions: #{missing.join(' ')}" if missing.length > 0
 end
